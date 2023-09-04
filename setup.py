637dbeb67be4a50f8cd1f5f0e7b3c1ead1d8ecbb
Adam.S <Bas-Man@users.noreply.github.com>
2023-09-04 13:52:36 +0900

    Add dependencies and ignore file

--- setup.py
+++ setup.py
@@ -6,14 +6,23 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        'google-api-python-client',
-        'google-auth-httplib2',
-        'google-auth-oauthlib',
+        'google-api-core==1.25.0',
+        'google-api-python-client==1.12.8',
+        'google-auth==1.24.0',
+        'google-auth-httplib2==0.0.4',
+        'google-auth-oauthlib==0.4.2',
+        'googleapis-common-protos==1.52.0',
         'line-notify==0.1.4',
-        'python-dotenv==0.21.1',
         'toml==0.10.2',
         'tomli==2.0.1',
     ],
+    extras_require={
+        "dev": [
+            "pytest==7.4.0",
+            'python-dotenv==0.21.1',
+            # List development-specific dependencies here
+        ],
+    },
     entry_points={
         'console_scripts': [
             'g2line = gmail2line.main:cli',
