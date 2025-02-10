7a4c555828352a71dad4e90707b68d76dad7e9db
Adam.S <Bas-Man@users.noreply.github.com>
2025-02-10 09:30:01 +0900

    message_builder tests

--- tests/message_builder/test_call_function.py
+++ tests/message_builder/test_call_function.py
@@ -48,6 +48,20 @@ def test_call_institute_func():
     assert result == '駒沢大学教室 Notification for Tester.\n08月17日 16時50分 に入室しました'
 
 
+def test_call_bus_func():
+    data = {
+        'date': '11月20日',
+        'time': '15時27分',
+        'name': 'タマオアイビーローズ',
+        'busname': '渋24',
+        'destination': '渋谷駅',
+        'boardedat': '成育医療研究センター前',
+        'notifier': 'bus',
+    }
+
+    result = build_message('Tester', data)
+    assert result == ""
+
 def test_call_no_function():
     data = {
         'date': '08月20日',
