2d606f3f649a5998da784822f5dbbb40b4940266
Adam.S <Bas-Man@users.noreply.github.com>
2025-02-10 09:25:48 +0900

    update train regex and location

---
+++ src/gmail2line/core/patterns/jpn/regex.py
@@ -0,0 +1,3 @@
+# Train Address: tskids@goopas.jp triggered by Pasmo
+train = r'(?P<date>\d{2}月\d{2}日)\s(?P<time>\d{2}時\d{2}分)(?:\r)?\n(?P<name>.*)さん(?:\r)?\n「(?P<provider>[一-龯]+)・(?P<station>[一-龯]+)」を(?P<enterexit>[一-龯]+)'
+# Bus Address: noreply@tskids.jp triggered by Pasmo
