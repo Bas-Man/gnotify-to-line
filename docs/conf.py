abed533ee8170f948efb157928f835112ae568ec
Adam.S <Bas-Man@users.noreply.github.com>
2025-02-27 12:39:12 +0900

    chore: Update conf.py
    Changed the theme for both local html and ReadTheDocs site

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -45,8 +47,12 @@ language = "en"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
-html_theme = "classic"
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'alabaster'
+
 html_static_path = ["_static"]
 
 # -- Options for todo extension ----------------------------------------------
